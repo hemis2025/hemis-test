01b552bda47de7ce86bdc9b0c6c0001d4c8a68c5
hemis2025 <neytronpazitron@gmail.com>
2025-08-27 20:01:29 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+// data.js
+const QA = [
+  {
+    id: 1,
+    q: "O‘zbekiston poytaxti qaysi?",
+    options: ["Samarqand", "Toshkent", "Buxoro", "Urganch"],
+    answer: "Toshkent"
+  },
+  {
+    id: 2,
+    q: "Amir Temur qaysi asrda yashagan?",
+    options: ["XIV asr", "XVI asr", "XVIII asr", "X asr"],
+    answer: "XIV asr"
+  }
+];
